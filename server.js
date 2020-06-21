8a73b9fbac5fdca17e2c72c42d8638de841c5e39
safinsingh <safin.singh@gmail.com>
2020-06-21 11:24:05 -0700

    prepare everything for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
 
 const items = require('./routes/api/items')
 
@@ -17,6 +18,15 @@ mongoose
 
 // Use Routes
 app.use('/api/items', items)
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Server started on port ${port}`))
